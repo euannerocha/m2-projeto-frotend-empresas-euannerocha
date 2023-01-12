345a9f56d0b315817275d958cf7ecdaee4b557c2
Annelise Rocha <annelisesr@gmail.com>
2023-01-12 02:01:03 -0300

    homepage - css e html

---
+++ src/scripts/homepage.js
@@ -0,0 +1,43 @@
+function openMenuLogin(){
+
+    let menuAsideContent = document.querySelector(".menuAsideContent") 
+    let openBtn = document.querySelector(".openBtn") 
+    
+console.log(openBtn)
+console.log(menuAsideContent)
+
+    openBtn.addEventListener('click', ()=>{     
+        menuAsideContent.style.display = `flex`
+    })
+
+}
+
+function closeMenuLogin(){
+    let menuAsideContent = document.querySelector(`.menuAsideContent`) 
+    let closeBtn = document.querySelector(`.closeBtn`) 
+
+    closeBtn.addEventListener('click', ()=>{
+        menuAsideContent.style.display = `none`
+    })
+}
+
+function goToLogin(){
+    let loginBtn = document.querySelector('.goToLogin')
+
+    loginBtn.addEventListener('click', ()=>{
+        window.location.replace('../../src/index/login.html')
+    })
+}
+
+function goToCadastro(){
+    let cadastroBtn = document.querySelector('.goToCadastro')
+
+    cadastroBtn.addEventListener('click', ()=>{
+        window.location.replace('../../src/index/register.html')
+    })
+}
+
+openMenuLogin()
+closeMenuLogin()
+goToLogin()
+goToCadastro()
