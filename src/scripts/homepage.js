0f0f5a89634a2bf49f119541acfb8ca5ba9f7cdf
Annelise Rocha <annelisesr@gmail.com>
2023-01-12 23:33:05 -0300

    Renderização das empresas em tela, css e html da homepage estão prontos

--- src/scripts/homepage.js
+++ src/scripts/homepage.js
@@ -3,8 +3,8 @@ function openMenuLogin(){
     let menuAsideContent = document.querySelector(".menuAsideContent") 
     let openBtn = document.querySelector(".openBtn") 
     
-console.log(openBtn)
-console.log(menuAsideContent)
+// console.log(openBtn)
+// console.log(menuAsideContent)
 
     openBtn.addEventListener('click', ()=>{     
         menuAsideContent.style.display = `flex`
@@ -41,3 +41,88 @@ openMenuLogin()
 closeMenuLogin()
 goToLogin()
 goToCadastro()
+
+
+
+
+async function requestSetores(){
+    let setores = await fetch('http://localhost:6278/sectors')
+    .then(resp => {
+     return resp.json()
+    })
+    .then(osta => {
+        // console.log(osta)
+
+        let optionAppend = document.getElementById('setores')
+
+        osta.forEach(element => {
+            let sector = document.createElement('option')
+            sector.innerText = element.description
+            sector.classList.add(element.description)
+            sector.value = element.description
+
+            optionAppend.append(sector)
+        })
+    })
+    .catch(error => {
+        console.log(`Ops! Ocorreu o seguinte erro: ${error}`)
+    })
+
+}
+
+async function requestEmpresas(){
+
+    let empresasPorSetor = await fetch('http://localhost:6278/companies')
+    .then(resp => {
+        return resp.json()
+       })
+    .then(osta => {
+           console.log(osta)
+
+           let appendCards = document.querySelector('.ulCardsList')
+
+           osta.forEach(element =>{
+                let liCard = document.createElement('li')
+                    let title = document.createElement('h1')
+                    let hours = document.createElement('p')
+                    let tagSetor = document.createElement('p')
+
+                    title.innerText = element.name
+                    hours.innerText = element.opening_hours
+                    tagSetor.innerText = element.sectors.description
+
+                    liCard.classList.add('liCard')
+                    title.classList.add('title')
+                    hours.classList.add('hours')
+                    tagSetor.classList.add('tagSetor')
+
+                appendCards.append(liCard)
+                liCard.append(title, hours, tagSetor)
+           })
+       })
+    .catch(error => {
+        console.log(`Ops! Ocorreu o seguinte erro: ${error}`)
+    })
+
+}
+
+// async function requestEmpresasPorSetor(name = ""){
+
+
+//     let empresasPorSetor = (await fetch(`http://localhost:6278/companies/${name}`))
+//     .then(resp => {
+//         return resp.json()
+//        })
+//     .then(osta => {
+//            console.log(osta)
+//        })
+       
+//     console.log(empresasPorSetor)
+       
+// } 
+    
+    
+requestSetores()
+requestEmpresas()
+// requestEmpresasPorSetor()
+
